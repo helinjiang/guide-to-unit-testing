41a86d2e3d30535078b6ced3e51a7275d2dee1c7
helinjiang <me@helinjiang.com>
2018-12-11 11:07:31 +0800

    feat: 使用 chai plugin

---
+++ test/json-file.test.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const chai = require('chai');
+const expect = chai.expect;
+
+chai.use(require('chai-json'));
+
+describe.only('try chai plugin: chai-json', () => {
+    it('package.json is a json file', () => {
+        expect(path.join(__dirname, '../package.json')).to.be.a.jsonFile();
+    });
+
+    it('README.md is a json file', () => {
+        expect(path.join(__dirname, '../README.md')).to.be.not.a.jsonFile();
+    });
+});
