0b00adaeaed683211fc394dc600c51dccb4329a7
helinjiang <me@helinjiang.com>
2018-12-11 10:17:05 +0800

    feat: 优化函数

--- src/add.js
+++ src/add.js
@@ -1,3 +1,5 @@
-module.exports = (a, b) => {
+function add(a, b) {
     return a + b;
-};
+}
+
+module.exports = add;
--- test/add_assert.test.js
+++ test/add_assert.test.js
@@ -1,9 +1,14 @@
+// https://www.chaijs.com/api/assert/
 const assert = require('chai').assert;
 
 const add = require('../src/add');
 
 describe('use assert: src/add.js', () => {
-    it('add(1,2) === 3', () => {
+    it('assert.equal: add(1,2) === 3', () => {
         assert.equal(add(1, 2), 3);
+    });
+
+    it('assert: add(1,2) === 3', () => {
+        assert(add(1, 2) === 3);
     });
 });
